285b13771b6691f537a82355a13895f97d92d704
Hugo Tonelli <hugotonelli@gmail.com>
2020-07-28 09:21:32 -0300

    Add project files.

---
+++ TeamsCLI/Graph/GraphHelper.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsCLI
+{
+    class GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                // GET /me
+                return await graphClient.Me.Request().GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            try
+            {
+                // GET /me/events
+                var resultPage = await graphClient.Me.Events.Request()
+                    // Only return the fields used by the application
+                    .Select(e => new
+                    {
+                        e.Subject,
+                        e.Organizer,
+                        e.Start,
+                        e.End
+                    })
+                    // Sort results by when they were created, newest first
+                    .OrderBy("createdDateTime DESC")
+                    .GetAsync();
+
+                return resultPage.CurrentPage;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting events: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<Chat>> GetChatsAsync()
+        {
+            try
+            {
+                // GET /me/chats
+                var resultPage = await graphClient.Me.Chats.Request()
+                    .Select(c => new
+                    {
+                        c.Id,
+                        c.Topic,
+                        //c.CreatedDateTime,
+                        //c.LastUpdatedDateTime,
+                        //c.Members
+                    })
+                    .GetAsync();
+
+                /* // Too much overhead results in forbidden requests error.
+                if (resultPage.CurrentPage != null)
+                {
+                    foreach (var chat in resultPage.CurrentPage)
+                    {
+                        if (chat.Members == null)
+                        {
+                            chat.Members = await graphClient.Chats[chat.Id].Members.Request().GetAsync();
+                        }
+                    }
+                }
+                */
+
+                return resultPage.CurrentPage;
+
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting chats: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<ConversationMember>> GetChatMembers(string chatId)
+        {
+            try
+            {
+                var resultPage = await graphClient.Chats[chatId].Members.Request().GetAsync();
+
+                return resultPage.CurrentPage;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting members for chat {chatId}: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ TeamsCLI/Program.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace TeamsCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teams CLI\n");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var tenantId = appConfig["tenantId"];
+            var scopes = scopesString.Split(';');
+
+            // Initialize the auth provider with values from appsettings.json
+            var authProvider = new DeviceCodeAuthProvider(appId, scopes, tenantId);
+
+            // Request a token to sign in the user
+            var accessToken = authProvider.GetAccessToken().Result;
+
+            // Initialize Graph client
+            GraphHelper.Initialize(authProvider);
+
+            // Get signed-in user
+            var user = GraphHelper.GetMeAsync().Result;
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Please choose one of the following options:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Display access token");
+                Console.WriteLine("2. List calendar events");
+                Console.WriteLine("3. List top 10 chats");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    // Set to invalid value
+                    choice = -1;
+                }
+
+                switch(choice)
+                {
+                    case 0:
+                        // Exit the program
+                        Console.WriteLine("Goodbye");
+                        break;
+                    case 1:
+                        // Display access token
+                        Console.WriteLine($"Access token: {accessToken}\n");
+                        break;
+                    case 2:
+                        // List the calendar
+                        ListCalendarEvents();
+                        break;
+                    case 3:
+                        // List chats
+                        ListChats();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            // Check for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]) ||
+                string.IsNullOrEmpty(appConfig["tenantId"]))
+            {
+                return null;
+            }
+
+            return appConfig;
+        }
+
+        static string FormatDateTimeTimeZone(Microsoft.Graph.DateTimeTimeZone value)
+        {
+            // Get the timezone specified in the Graph value
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById(value.TimeZone);
+            // Parse the date/time string from Graph into a DateTime
+            var dateTime = DateTime.Parse(value.DateTime);
+
+            // Create a DateTimeOffset in the specific timezone indicated by Graph
+            var dateTimeWithTZ = new DateTimeOffset(dateTime, timeZone.BaseUtcOffset)
+                .ToLocalTime();
+
+            return dateTimeWithTZ.ToString("g");
+        }
+
+        static void ListCalendarEvents()
+        {
+            var events = GraphHelper.GetEventsAsync().Result;
+
+            if (events == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Events: ");
+
+            foreach (var calendarEvent in events)
+            {
+                Console.WriteLine($"Subject: {calendarEvent.Subject}");
+                Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
+                Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start)}");
+                Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End)}");
+            }
+        }
+
+        static void ListChats()
+        {
+            var chats = GraphHelper.GetChatsAsync().Result;
+
+            if (chats == null)
+            {
+                return;
+            }
+
+            chats = chats.Take(10);
+
+            Console.WriteLine("Chats: ");
+
+            foreach (var chat in chats)
+            {
+                Console.WriteLine($"Chat: {chat.Id}");
+                Console.WriteLine($"  Topic: {chat.Topic}");
+                //Console.WriteLine($"  CreatedDateTime: {chat.CreatedDateTime}");
+                //Console.WriteLine($"  LastUpdatedDateTime: {chat.LastUpdatedDateTime}");
+                ListChatMembers(chat.Id);
+            }
+        }
+
+        static void ListChatMembers(string chatId)
+        {
+            var chatMembers = GraphHelper.GetChatMembers(chatId).Result;
+
+            if (chatMembers != null)
+            {
+                Console.WriteLine($"  Members: {String.Join(", ", chatMembers.Select(m => m.DisplayName))}");
+            }
+        }
+    }
+}
